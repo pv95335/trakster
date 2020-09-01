e6ef1309381a978b4ebe67c6ba7ab58366026842
Natalia Teaca <lilikoy@sbcglobal.net>
2020-09-01 13:15:01 -0700

    working logout

--- public/javascript/create-post.js
+++ public/javascript/create-post.js
@@ -1,37 +1,32 @@
 async function newFormHandler(event) {
-    // event.preventDefault();
+	event.preventDefault();
 
-    const title = document.querySelector('#post-title').value;
-    const post_content = document.querySelector('#post-content').value;
-    const price = document.querySelector('#post-price').value;
-    const area = document.querySelector('#post-area').value;
-    const  post_type = document.querySelector('#post-type').value;
-   console.log(title);
-   console.log(post_content);
-   console.log(price);
-    console.log(area);
-    console.log(post_type)
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            post_content,
-            price,
-            area,
-            post_type
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+	const title = document.querySelector('#post-title').value;
+	const post_content = document.querySelector('#post-content').value;
+	const price = document.querySelector('#post-price').value;
+	const area = document.querySelector('#post-area').value;
+	const post_type = document.querySelector('#post-type').value;
+	const response = await fetch(`/api/posts`, {
+		method: 'POST',
+		body: JSON.stringify({
+			title,
+			post_content,
+			price,
+			area,
+			post_type
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 
-    if (response.ok) {
-        console.log('added');
-        document.location.replace('/');
-    } else {
-        alert(response.statusText);
-    }
+	if (response.ok) {
+		console.log('added');
+		document.location.replace('/');
+	} else {
+		alert(response.statusText);
+	}
 }
 
 
-    document.querySelector('#newpost-btn').addEventListener('submit', newFormHandler);
+document.querySelector('#createPost').addEventListener('submit', newFormHandler);
